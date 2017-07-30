244010d76a83cb7516e3c857530d4300ba869552
Joseph Stevens <josephscottstevens@gmail.com>
2017-07-30 12:46:46 -0400

    bugfix for 12x12 grid

--- lab/gridApp.fsx
+++ lab/gridApp.fsx
@@ -1,13 +1,21 @@
 let getGridStr m n =
     let addTab x y = x + "\t" + y                            // Takes in two strings, add's a "\t" between them
     let addNewLine x y = x + "\n" + y                        // Takes in two strings, add's a "\n" between them
+    let formatIntAsString (x:int) =                          // Formats an int as a string, 
+        let padding =
+            if x >= 100 then                                 // Example x = 100
+                ""                                           // Return: "100"
+            else if x >= 10 then                             // Example x = 23
+                " "                                          // Return: " 023"
+            else                                             // Example x = 7
+                "  "                                         // Return: "0 7"
+        padding + string x
     let formatInnerString (x: string) =                      // Takes a string, makes a new string prepends the first character of the original string, 
-        string (x.[0]) + "| " + x                            //  and a "| " to the original string
+        string (x.[..2]) + "| " + x                            //  and a "| " to the original string
 
     let createInnerString x =                                // Creates a sequence from an integer, ie
-        [x .. x .. n * x]                                    // Given 1, return: [1; 2; 3; 4; 5;] 
-                                                             // Or Given 2, return [2; 4; 6; 8; 10;]
-        |> List.map string                                   // Maps the above list of ints, to strings
+        [x .. x .. n * x]                                    // Given 1, return: [1; 2; 3; 4; 5;] Or Given 2, return [2; 4; 6; 8; 10;]
+        |> List.map formatIntAsString                        // Maps the above list of ints, to strings
         |> List.reduce addTab                                // reduces them together with a tab between each string
 
     let gridStrMain = 
@@ -17,12 +25,13 @@ let getGridStr m n =
 
     let header =                                             // Creates the header row for the output
         [1..n]                                               // Creates a list of ints from 1 to n
-        |> Seq.map string                                    // Maps each Int to a string
+        |> Seq.map formatIntAsString                         // Maps each Int to a string
         |> Seq.reduce addTab                                 // Reduces them together with a tab between each character
 
-    "X  " + header + "\n\n" + gridStrMain
+    "  X    " + header + "\n\n" + gridStrMain
 
 let get12X12GridStr() =
+    System.Windows.Forms.Clipboard.SetText(getGridStr 12 12)
     printfn "%s" (getGridStr 12 12)    
 
 let getManualGridString() =
